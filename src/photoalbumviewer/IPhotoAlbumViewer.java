284dc64835c50ff3f6ab008724cfd3c8e428dae8
anzyu-neu <yu.andy1@northeastern.edu>
2023-04-17 23:33:16 -0400

    started implementation for graphics

--- src/photoalbumviewer/IPhotoAlbumViewer.java
+++ src/photoalbumviewer/IPhotoAlbumViewer.java
@@ -1,15 +1,20 @@
 package photoalbumviewer;
 
 import java.util.List;
-
 import photoalbum.Snapshot;
 
 /**
  * Interface to view the photo album page.
  */
 public interface IPhotoAlbumViewer {
+
+  /**
+   * Takes in file and reads parameters for photo page setup.
+   */
+  void readFile();
   List<String> listSnapshots();
   Snapshot nextSnapshot();
   Snapshot previousSnapshot();
   Snapshot loadSnapshot();
+
 }
--- src/photoalbumviewer/PhotoAlbumGraphical.java
+++ src/photoalbumviewer/PhotoAlbumGraphical.java
@@ -1,5 +1,6 @@
 package photoalbumviewer;
 
+import java.io.File;
 import java.util.List;
 import javax.swing.JFrame;
 import javax.swing.JButton;
@@ -12,7 +13,30 @@ import photoalbum.Snapshot;
  */
 public class PhotoAlbumGraphical implements IPhotoAlbumViewer {
   private PhotoPage page;
+  private File file;
+  private static int WIDTH;
+  private static int HEIGHT;
 
+  /**
+   * Constructs the graphical view instance for rendering.
+   * @param file name of file with page descriptions to create.
+   * @param xMax width of canvas
+   * @param yMax height of canvas
+   */
+  public PhotoAlbumGraphical(File file, int xMax, int yMax) {
+    if (!file.exists()) {
+      throw new IllegalArgumentException("File does not exist.");
+    }
+    this.page = PhotoPage.getPage();
+    this.file = file;
+    this.WIDTH = xMax;
+    this.HEIGHT = yMax;
+  }
+
+  @Override
+  public void readFile() {
+    return;
+  }
 
   @Override
   public List<String> listSnapshots() {
